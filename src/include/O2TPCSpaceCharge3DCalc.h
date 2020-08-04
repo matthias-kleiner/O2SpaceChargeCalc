3a25e513f2705370112fbe7786387b8200c6cf56
Matthias Kleiner <Matthias.Kleiner@cern.ch>
2020-08-04 14:58:41 +0200

    moved structs to own file

--- src/include/O2TPCSpaceCharge3DCalc.h
+++ src/include/O2TPCSpaceCharge3DCalc.h
@@ -18,6 +18,7 @@
 
 #include "TriCubic.h"
 #include "AliRoot/AliTPCPoissonSolver.h"
+#include "SpaceChargeStructs.h"
 
 // Root includes
 #include "TF1.h" /// for numerical intergration only
@@ -28,176 +29,6 @@
 
 // temporary for nearest neighbour search
 #include "GeometricalTools/NearestNeighborQuery.h"
-
-template <typename DataT = float>
-struct AnalyticalFields {
-
-  DataT parA{1e-5};                     ///< parameter [0] of functions
-  DataT parB{0.5};                      ///< parameter [1] of functions
-  DataT parC{1e-4};                     ///< parameter [2] of functions
-  static constexpr unsigned int id = 0; ///< needed to distinguish between the differrent structs
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for electric field Er for given coordinate
-  DataT evalEr(DataT z, DataT r, DataT phi) const
-  {
-    return erFunc(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for electric field Ez for given coordinate
-  DataT evalEz(DataT z, DataT r, DataT phi) const
-  {
-    return ezFunc(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for electric field Ephi for given coordinate
-  DataT evalEphi(DataT z, DataT r, DataT phi) const
-  {
-    return ephiFunc(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for the potential for given coordinate
-  DataT evalPotential(DataT z, DataT r, DataT phi) const
-  {
-    return potentialFunc(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for the space charge density for given coordinate
-  DataT evalDensity(DataT z, DataT r, DataT phi) const
-  {
-    return densityFunc(z, r, phi);
-  }
-
-  /// analytical potential
-  std::function<DataT(DataT, DataT, DataT)> potentialFunc = [& parA = parA, &parB = parB, &parC = parC](DataT z, DataT r, DataT phi) {
-    return -parA * (std::pow((-r + 254.5 + 83.5), 4) - 338.0 * std::pow((-r + 254.5 + 83.5), 3) + 21250.75 * std::pow((-r + 254.5 + 83.5), 2)) * std::cos(parB * phi) * std::cos(parB * phi) * std::exp(-1 * parC * (z - 125) * (z - 125));
-  };
-
-  /// analytical space charge - NOTE: if the space charge density is calculated analytical there would be a - sign in the formula (-parA)  - however since its an e- the sign is flipped (IS THIS CORRECT??? see for minus sign: AliTPCSpaceCharge3DCalc::SetPotentialBoundaryAndChargeFormula)-
-  std::function<DataT(DataT, DataT, DataT)> densityFunc = [& parA = parA, &parB = parB, &parC = parC](DataT z, DataT r, DataT phi) {
-    return parA * ((1 / r * 16 * (-3311250 + 90995.5 * r - 570.375 * r * r + r * r * r)) * std::cos(parB * phi) * std::cos(parB * phi) * std::exp(-1 * parC * (z - 125) * (z - 125)) +
-                   (std::pow(-r + 254.5 + 83.5, 4) - 338.0 * std::pow(-r + 254.5 + 83.5, 3) + 21250.75 * std::pow(-r + 254.5 + 83.5, 2)) / (r * r) * std::exp(-1 * parC * (z - 125) * (z - 125)) * -2 * parB * parB * std::cos(2 * parB * phi) +
-                   (std::pow(-r + 254.5 + 83.5, 4) - 338.0 * std::pow(-r + 254.5 + 83.5, 3) + 21250.75 * std::pow(-r + 254.5 + 83.5, 2)) * std::cos(parB * phi) * std::cos(parB * phi) * 2 * parC * std::exp(-1 * parC * (z - 125) * (z - 125)) * (2 * parC * (z - 125) * (z - 125) - 1));
-  };
-
-  /// analytical electric field Er
-  std::function<DataT(DataT, DataT, DataT)> erFunc = [& parA = parA, &parB = parB, &parC = parC](DataT z, DataT r, DataT phi) {
-    return parA * 4 * (r * r * r - 760.5 * r * r + 181991 * r - 1.3245 * std::pow(10, 7)) * std::cos(parB * phi) * std::cos(parB * phi) * std::exp(-1 * parC * (z - 125) * (z - 125));
-  };
-
-  /// analytical electric field Ephi
-  std::function<DataT(DataT, DataT, DataT)> ephiFunc = [& parA = parA, &parB = parB, &parC = parC](DataT z, DataT r, DataT phi) {
-    return parA * (std::pow(-r + 254.5 + 83.5, 4) - 338.0 * std::pow(-r + 254.5 + 83.5, 3) + 21250.75 * (-r + 254.5 + 83.5) * (-r + 254.5 + 83.5)) / r * std::exp(-1 * parC * (z - 125) * (z - 125)) * -parB * std::sin(2 * parB * phi);
-  };
-
-  /// analytical electric field Ez
-  std::function<DataT(DataT, DataT, DataT)> ezFunc = [& parA = parA, &parB = parB, &parC = parC](DataT z, DataT r, DataT phi) {
-    return parA * (std::pow(-r + 254.5 + 83.5, 4) - 338.0 * std::pow(-r + 254.5 + 83.5, 3) + 21250.75 * (-r + 254.5 + 83.5) * (-r + 254.5 + 83.5)) * std::cos(parB * phi) * std::cos(parB * phi) * -2 * parC * (z - 125) * std::exp(-1 * parC * (z - 125) * (z - 125));
-  };
-};
-
-template <typename DataT = float, typename Grid3D = RegularGrid3D<>>
-struct NumericalFields {
-  // using RegularGrid = RegularGrid3D<DataT, Nz, Nr, Nphi>;
-
-  NumericalFields(const Grid3D& gridErTmp, const Grid3D& gridEzTmp, const Grid3D& gridEphiTmp) : gridEr{gridErTmp}, gridEz{gridEzTmp}, gridEphi{gridEphiTmp} {};
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for electric field Er for given coordinate
-  DataT evalEr(DataT z, DataT r, DataT phi) const
-  {
-    return interpolatorEr(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for electric field Ez for given coordinate
-  DataT evalEz(DataT z, DataT r, DataT phi) const
-  {
-    return interpolatorEz(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for electric field Ephi for given coordinate
-  DataT evalEphi(DataT z, DataT r, DataT phi) const
-  {
-    return interpolatorEphi(z, r, phi);
-  }
-
-  const Grid3D& gridEr{};   // adress to the data container of the grid
-  const Grid3D& gridEz{};   // adress to the data container of the grid
-  const Grid3D& gridEphi{}; // adress to the data container of the grid
-  const bool circularZ = false;
-  const bool circularR = false;
-  const bool circularPhi = true;
-  TriCubicInterpolator<DataT, Grid3D> interpolatorEr{gridEr, circularZ, circularR, circularPhi};
-  TriCubicInterpolator<DataT, Grid3D> interpolatorEz{gridEz, circularZ, circularR, circularPhi};
-  TriCubicInterpolator<DataT, Grid3D> interpolatorEphi{gridEphi, circularZ, circularR, circularPhi};
-  static constexpr unsigned int id = 1; ///< needed to distinguish between the differrent structs
-};
-
-template <typename DataT = float, typename Grid3D = RegularGrid3D<>>
-struct DistCorrInterpolator {
-
-  DistCorrInterpolator(const Grid3D& lDistCorrdR, const Grid3D& lDistCorrdZ, const Grid3D& lDistCorrdRPhi) : mDistCorrdR{lDistCorrdR}, mDistCorrdZ{lDistCorrdZ}, mDistCorrdRPhi{lDistCorrdRPhi} {};
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for the local distortion or correction dR for given coordinate
-  DataT evaldR(DataT z, DataT r, DataT phi) const
-  {
-    return mInterpolatorDistCorrdR(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for the local distortion or correction dZ for given coordinate
-  DataT evaldZ(DataT z, DataT r, DataT phi) const
-  {
-    return mInterpolatorDistCorrdZ(z, r, phi);
-  }
-
-  /// \param r r coordinate
-  /// \param phi phi coordinate
-  /// \param z z coordinate
-  /// \return returns the function value for the local distortion or correction dRPhi for given coordinate
-  DataT evaldRPhi(DataT z, DataT r, DataT phi) const
-  {
-    return mInterpolatorDistCorrdRPhi(z, r, phi);
-  }
-
-  const Grid3D& mDistCorrdR{};    // adress to the data container of the grid
-  const Grid3D& mDistCorrdZ{};    // adress to the data container of the grid
-  const Grid3D& mDistCorrdRPhi{}; // adress to the data container of the grid
-  const bool circularZ = false;
-  const bool circularR = false;
-  const bool circularPhi = true;
-  TriCubicInterpolator<DataT, Grid3D> mInterpolatorDistCorrdR{mDistCorrdR, circularZ, circularR, circularPhi};
-  TriCubicInterpolator<DataT, Grid3D> mInterpolatorDistCorrdZ{mDistCorrdZ, circularZ, circularR, circularPhi};
-  TriCubicInterpolator<DataT, Grid3D> mInterpolatorDistCorrdRPhi{mDistCorrdRPhi, circularZ, circularR, circularPhi};
-  static constexpr unsigned int id = 2; ///< needed to distinguish between the differrent structs
-};
 
 /// \tparam DataT the type of data which is used during the calculations
 /// \tparam Nr number of vertices in r direction
@@ -958,7 +789,7 @@ void O2TPCSpaceCharge3DCalc<DataT, Nr, Nz, Nphi>::calcGlobalDistWithGlobalCorrIt
   const int maxLevel = 10;
   gte::NearestNeighborQuery<3, float, gte::PositionSite<3, float>> kdTree(sites, maxLeafSize, maxLevel);
 
-  #pragma omp parallel for
+#pragma omp parallel for
   for (unsigned int iZ = 0; iZ < Nz; ++iZ) {
     for (unsigned int iR = 0; iR < Nr; ++iR) {
       for (unsigned int iPhi = 0; iPhi < Nphi; ++iPhi) {
@@ -1010,7 +841,7 @@ void O2TPCSpaceCharge3DCalc<DataT, Nr, Nz, Nphi>::calcGlobalDistWithGlobalCorrIt
         DataT corrdRPhi = 0;
         DataT corrdZ = 0;
 
-        for(int iter = 0; iter<maxIter; ++iter){
+        for (int iter = 0; iter < maxIter; ++iter) {
           // 2. get new points coordinates
           const DataT rPos = getRVertex(nearestiR) + stepR;
           const DataT zPos = getZVertex(nearestiZ) + stepZ;
@@ -1034,13 +865,13 @@ void O2TPCSpaceCharge3DCalc<DataT, Nr, Nz, Nphi>::calcGlobalDistWithGlobalCorrIt
           stepPhi += distancePhi * phiStepWidth;
 
           const DataT totaldistRDiv = lastCorrdR == 0 ? 0 : 1 - std::abs(distanceR / lastCorrdR); // should be larger than 0
-          const bool checkR = totaldistRDiv <= convR; // if the improvemnt in distance is smaller than changeRFactor set the flag
+          const bool checkR = totaldistRDiv <= convR;                                             // if the improvemnt in distance is smaller than changeRFactor set the flag
 
           const DataT totaldistZDiv = lastCorrdZ == 0 ? 0 : 1 - std::abs(distanceZ / lastCorrdZ); // should be larger than 0
-          const bool checkZ = totaldistZDiv <= convZ; // if the improvemnt in distance is smaller than changeRFactor set the flag
+          const bool checkZ = totaldistZDiv <= convZ;                                             // if the improvemnt in distance is smaller than changeRFactor set the flag
 
           const DataT totaldistPhiDiv = lastCorrdRPhi == 0 ? 0 : 1 - std::abs(distancePhi / lastCorrdRPhi); // should be larger than 0
-          const bool checkPhi = totaldistPhiDiv <= convPhi; // if the improvemnt in distance is smaller than changeRFactor set the flag
+          const bool checkPhi = totaldistPhiDiv <= convPhi;                                                 // if the improvemnt in distance is smaller than changeRFactor set the flag
 
           if (checkR && checkZ && checkPhi) {
             break;

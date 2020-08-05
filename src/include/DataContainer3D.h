6e2ea9ee749f2ec67ccda1ec10930ae1754d606d
Matthias Kleiner <Matthias.Kleiner@cern.ch>
2020-08-05 16:19:24 +0200

    cleaned DataContainer3D.h

--- src/include/DataContainer3D.h
+++ src/include/DataContainer3D.h
@@ -6,11 +6,15 @@
 #include <iostream>
 #include "TFile.h"
 
+/// \tparam DataT the type of data which is used during the calculations
+/// \tparam Nx number of vertices in x direction
+/// \tparam Ny number of vertices in y direction
+/// \tparam Nz number of vertices in z direction
 template <typename DataT = float, unsigned int Nx = 4, unsigned int Ny = 4, unsigned int Nz = 4>
 struct DataContainer3D {
 
-  static constexpr size_t FNdataPoints{Nx * Ny * Nz}; ///< number of values stored in the container
-  std::unique_ptr<DataT[]> mData = std::make_unique<DataT[]>(FNdataPoints);
+  static constexpr size_t FNdataPoints{Nx * Ny * Nz};                       ///< number of values stored in the container
+  std::unique_ptr<DataT[]> mData = std::make_unique<DataT[]>(FNdataPoints); ///< storage for the data
 
   const DataT& operator[](size_t i) const { return mData[i]; }
   DataT& operator[](size_t i) { return mData[i]; }
@@ -18,7 +22,6 @@ struct DataContainer3D {
   const DataT& operator()(size_t ix, size_t iy, size_t iz) const
   {
     const size_t ind = getDataIndex(ix, iy, iz);
-
     return mData[ind];
   }
 
@@ -30,10 +33,7 @@ struct DataContainer3D {
 
   size_t getDataIndex(const size_t ix, const size_t iy, const size_t iz) const
   {
-    const size_t indX = ix;
-    const size_t indY = iy;
-    const size_t indZ = iz;
-    const size_t index = indX + Nx * (indY + indZ * Ny);
+    const size_t index = ix + Nx * (iy + iz * Ny);
     return index;
   }
 
@@ -44,7 +44,6 @@ struct DataContainer3D {
 
   int writeToFile(TFile& outf, const char* name = "data") const
   {
-
     if (outf.IsZombie()) {
       std::cout << "Failed to write to file " << outf.GetName() << std::endl;
       return -1;
@@ -57,7 +56,6 @@ struct DataContainer3D {
 
   inline static DataContainer3D<DataT, Nx, Ny, Nz>* readFromFile(TFile& inpf, const char* name = "data")
   {
-    /// read from file
     if (inpf.IsZombie()) {
       std::cout << "Failed to read from file " << inpf.GetName() << std::endl;
       return nullptr;
@@ -70,7 +68,6 @@ struct DataContainer3D {
       std::cout << "Failed to load " << name << " from " << inpf.GetName() << std::endl;
       return nullptr;
     }
-
     return dataCont;
   }
 
@@ -87,7 +84,7 @@ struct DataContainer3D {
     }
   }
 
-  //custom data streamer for static array
+  //custom data streamer for unique_ptr
   static constexpr void dataStreamer(TBuffer& buf, void* objPtr)
   {
     DataContainer3D<DataT, Nx, Ny, Nz>* dataCont = static_cast<DataContainer3D<DataT, Nx, Ny, Nz>*>(objPtr);
@@ -101,6 +98,5 @@ struct DataContainer3D {
   }
   ClassDefNV(DataContainer3D, 1)
 };
-
 
 #endif
